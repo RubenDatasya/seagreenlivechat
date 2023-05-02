231521f99e78b6730329080f94aec4b0412708a4
RubenDataSya <ruben@datasya.com>
2023-05-02 11:32:03 +0300

    API - Added expiration to token

--- TokenGenerator/src/main/kotlin/io/agora/sample/RtmTokenBuilderSample.java
+++ TokenGenerator/src/main/kotlin/io/agora/sample/RtmTokenBuilderSample.java
@@ -3,6 +3,8 @@ package io.agora.sample;
 import io.agora.rtm.RtmTokenBuilder;
 import io.agora.rtm.RtmTokenBuilder.Role;
 
+import java.util.Date;
+
 public class RtmTokenBuilderSample {
     String appId = "0089641598304276ab3e6baf141c0258";
     String certificate = "12e9058b7aa64cd6898f2ab446f3e31f";
@@ -25,7 +27,8 @@ public class RtmTokenBuilderSample {
 
     public String getToken(String userId) throws Exception  {
         RtmTokenBuilder token = new RtmTokenBuilder();
-        String result = token.buildToken(appId, certificate, userId, Role.Rtm_User, expireTimestamp);
+        int expiredTsInSeconds = 1446455471 + (int) (new Date().getTime()/1000l);
+        String result = token.buildToken(appId, certificate, userId, Role.Rtm_User, expiredTsInSeconds);
         System.out.println(result);
         return result;
     }
--- TokenGenerator/src/main/kotlin/io/agora/sample/SignalingTokenSample.java
+++ TokenGenerator/src/main/kotlin/io/agora/sample/SignalingTokenSample.java
@@ -22,7 +22,6 @@ public class SignalingTokenSample {
     public String getToken(String userId) throws Exception  {
         String appId = "0089641598304276ab3e6baf141c0258";
         String certificate = "12e9058b7aa64cd6898f2ab446f3e31f";
-        String account = "TestAccount";
         //Use the current time plus an available time to guarantee the only time it is obtained
         int expiredTsInSeconds = 1446455471 + (int) (new Date().getTime()/1000l);
         String result = SignalingToken.getToken(appId, certificate, userId, expiredTsInSeconds);
