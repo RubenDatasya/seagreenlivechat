ac7c5a32749f94b2ac1ada0afdacb60ffc9e886f
RubenDataSya <ruben@datasya.com>
2023-05-02 10:33:45 +0300

    token fetching update

--- TokenGenerator/src/main/kotlin/io/agora/sample/SignalingTokenSample.java
+++ TokenGenerator/src/main/kotlin/io/agora/sample/SignalingTokenSample.java
@@ -1,5 +1,6 @@
 package io.agora.sample;
 
+import io.agora.rtm.RtmTokenBuilder;
 import io.agora.signal.SignalingToken;
 
 import java.security.NoSuchAlgorithmException;
@@ -9,13 +10,23 @@ public class SignalingTokenSample {
 
     public static void main(String []args) throws NoSuchAlgorithmException{
 
-        String appId = "970ca35de60c44645bbae8a215061b33";
-        String certificate = "5cfd2fd1755d40ecb72977518be15d3b";
+        String appId = "0089641598304276ab3e6baf141c0258";
+        String certificate = "12e9058b7aa64cd6898f2ab446f3e31f";
         String account = "TestAccount";
         //Use the current time plus an available time to guarantee the only time it is obtained
         int expiredTsInSeconds = 1446455471 + (int) (new Date().getTime()/1000l);
         String result = SignalingToken.getToken(appId, certificate, account, expiredTsInSeconds);
         System.out.println(result);
+    }
 
+    public String getToken(String userId) throws Exception  {
+        String appId = "0089641598304276ab3e6baf141c0258";
+        String certificate = "12e9058b7aa64cd6898f2ab446f3e31f";
+        String account = "TestAccount";
+        //Use the current time plus an available time to guarantee the only time it is obtained
+        int expiredTsInSeconds = 1446455471 + (int) (new Date().getTime()/1000l);
+        String result = SignalingToken.getToken(appId, certificate, userId, expiredTsInSeconds);
+        System.out.println(result);
+        return result;
     }
 }
